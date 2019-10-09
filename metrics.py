23a3d9fe8255c169929b00d122b77c4fedfb5560
marco.nicolas.rodriguez <marco.nicolas.rodriguez@fing.edu.uy>
2019-10-09 16:35:51 -0300

    Add metrics.

--- metrics.py
+++ metrics.py
@@ -1,8 +1,10 @@
+from hyperparameters import *
 import numpy as np
-from tensorflow import keras
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+import tensorflow as tf
+from sklearn.metrics import f1_score, precision_score, recall_score
 
-class Metrics(keras.callbacks.Callback):
+
+class Metrics(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.val_f1s = []
         self.val_recalls = []
@@ -10,11 +12,7 @@ class Metrics(keras.callbacks.Callback):
         self.epoch = 0
 
     def on_epoch_end(self, epoch, logs={}):
-        if 'parallel' in self.file_name:
-            # assume sigmoid function used in the final activation value
-            val_predict = np.where(self.model.predict(self.validation_data[0], batch_size=32) > 0.5, 1, 0)
-        else:
-            val_predict = np.asarray(self.model.predict_classes(self.validation_data[0], batch_size=32))
+        val_predict = np.asarray(self.model.predict_classes(self.validation_data[0], batch_size=BATCH_SIZE))
         val_targ = self.validation_data[1]
         _val_f1 = f1_score(val_targ, val_predict)
         _val_precision = precision_score(val_targ, val_predict)
@@ -23,9 +21,5 @@ class Metrics(keras.callbacks.Callback):
         self.val_precisions.append(_val_precision)
         self.val_recalls.append(_val_recall)
         print(' — val_f1: %f — val_precision: %f — val_recall %f' % (_val_f1, _val_precision, _val_recall))
-        print('%2d,%f,%f,%f' % (self.epoch, _val_f1, _val_precision, _val_recall), file=open(self.file_name, 'a'))
         self.epoch += 1
         return
-
-    def set_file_name(self, file_name):
-        self.file_name = file_name
