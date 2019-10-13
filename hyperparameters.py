4b7efd4ae51fc97174398164018f067d03d1d427
marco.nicolas.rodriguez <marco.nicolas.rodriguez@fing.edu.uy>
2019-10-13 17:06:44 -0300

    Update and add hyperparameters.

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+ACTIVATION = 'relu'
 BATCH_SIZE = 32
+DROPOUT = 0.1
 EPOCHS = 10
-MAX_WORDS_PER_TWEET = 39
+KERNEL_INITIALIZER = 'glorot_uniform'
+LOSS = 'binary_crossentropy'
+MAX_WORDS_PER_TWEET = 30
+OPTIMIZER = 'adam'
+RECURRENT_DROPOUT = 0.1
 TOTAL_UNITS = 30
